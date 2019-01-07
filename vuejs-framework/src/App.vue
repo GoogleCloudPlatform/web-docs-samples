8e28128092692ecbc4e842c9169c1b7e49fc9e50
Carter Fort <carterfort@google.com>
2019-01-07 16:59:20 -0500

    Change name

---
+++ vuejs-framework/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/internal">Internal</router-link>
+    <router-view />
+  </div>
+</template>
+
+
---
+++ vuejs-framework/src/routes/index.js
@@ -0,0 +1,15 @@
+import Home from '@/pages/Home'
+import Internal from '@/pages/Internal'
+
+const routes = [
+    {
+        path : "/",
+        component : Home
+    },
+    {
+        path : "/internal",
+        component : Internal
+    }
+]
+
+export default routes;
